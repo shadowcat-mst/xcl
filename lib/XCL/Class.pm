c644a42c80e1c0c7170eeb655acc606b4d494ae9
Matt S Trout <mst@shadowcat.co.uk>
2019-11-07 01:31:30 +0000

    better strictures calling

--- lib/XCL/Class.pm
+++ lib/XCL/Class.pm
@@ -5,7 +5,7 @@ use Import::Into;
 
 sub import {
   Mu->import::into(1);
-  strictures->import(1, { version => 2 });
+  strictures->import::into(1, { version => 2 });
   experimental->import::into(1, 'signatures', 'state', 'say');
 }
 
