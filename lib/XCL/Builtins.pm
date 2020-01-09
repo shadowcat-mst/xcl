f0aea43f0c4f99a51ea1f8fcdc86fe5116a93227
Matt S Trout <mst@shadowcat.co.uk>
2020-01-09 02:17:35 +0000

    move if and while back into Builtins.pm

--- lib/XCL/Builtins.pm
+++ lib/XCL/Builtins.pm
@@ -20,4 +20,44 @@ sub c_fx_id ($class, $scope, $thing) {
   $thing->evaluate_against($scope);
 }
 
+async sub c_fx_if {
+  my ($class, $scope, $lst) = @_;
+  my ($cond, $true, $false) = @{$lst->data};
+  my $dscope = $scope->derive;
+  my $res = await $cond->evaluate_against($dscope);
+  return $res unless $res->is_ok;
+  my $bool = $res->val->bool;
+  return $bool unless $bool->is_ok;
+  if ($bool->val->data) {
+    my $res = await $true->evaluate_against($dscope);
+    return defined($false) ? $res : Val($res);
+  }
+  return await $false->evaluate_against($dscope) if $false;
+  return Val(Err([ Name('NO_SUCH_VALUE') => String('else') ]));
+}
+
+async sub c_fx_while {
+  my ($class, $scope, $lst) = @_;
+  my ($cond, $body) = $lst->values;
+  my $dscope = $scope->derive;
+  my $did;
+  WHILE: while (1) {
+    my $res = await $cond->evaluate_against($dscope);
+    return $res unless $res->is_ok;
+    my $bool = $res->val->bool;
+    return $bool unless $bool->is_ok;
+    if ($bool->val->data) {
+      $did ||= 1;
+      my $bscope = $dscope->derive;
+      my $res = await $body->evaluate_against($bscope);
+      return $res unless $res->is_ok;
+    } else {
+      last WHILE;
+    }
+  }
+  return Val(Bool($did));
+}
+
+1;
+
 1;
