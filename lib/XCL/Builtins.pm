c5a0d5c6064a1869e76a894582f12c5657913d63
Matt S Trout <mst@shadowcat.co.uk>
2020-01-09 02:13:11 +0000

    remove old builtin expansion that doesn't work, see sketches/vmsketch for original

--- lib/XCL/Builtins.pm
+++ lib/XCL/Builtins.pm
@@ -20,40 +20,4 @@ sub c_fx_id ($class, $scope, $thing) {
   $thing->evaluate_against($scope);
 }
 
-my $expand_ns = sub ($pkg, $ns) {
-  map +($_ => Var(Native($pkg->can($_)))),
-    grep /^[a-z]/,
-    grep $pkg->can($_),
-    CORE::keys %$ns;
-};
-
-use XCL::Values;
-
-our $Builtins = Environment(Dict {
-  $expand_ns->('XCL::Builtins', \%XCL::Builtins::),
-  Call => Val(Environment(
-    Dict(+{ $expand_ns->('XCL::Builtins::Call', \%XCL::Builtins::Call::) })
-  )),
-  List => Val(Environment(
-    Dict(+{ $expand_ns->('XCL::Builtins::List', \%XCL::Builtins::List::) })
-  )),
-  Name => Val(Environment(
-    Dict(+{ $expand_ns->('XCL::Builtins::Name', \%XCL::Builtins::Name::) })
-  )),
-  Dict => Val(Environment(
-    Dict(+{ $expand_ns->('XCL::Builtins::Dict', \%XCL::Builtins::Dict::) })
-  )),
-});
-
-{
-  my $builtins = $Builtins->data->data;
-  my %ops = (
-    %XCL::Builtins::ops,
-    add => '+', multiply => '*', subtract => '-', divide => '/',
-    id => '$', dict => '%',
-  );
-  $builtins->{$ops{$_}} = $builtins->{$_}
-    for CORE::keys %ops;
-}
-
 1;
