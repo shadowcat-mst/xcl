395707ac0829d169f7e3d45a6bfd8be70d7ccfdc
Matt S Trout <mst@shadowcat.co.uk>
2020-02-08 03:05:56 +0000

    accidentally had boolification of numbers inverted

--- lib/XCL/V/Int.pm
+++ lib/XCL/V/Int.pm
@@ -5,7 +5,7 @@ use XCL::Class 'XCL::V';
 
 with 'XCL::V::Role::Numeric';
 
-sub bool ($self) { ValF(Bool($self->data == 0 ? 1 : 0)) }
+sub bool ($self) { ValF(Bool($self->data == 0 ? 0 : 1)) }
 
 sub display_data ($self, $) { ''.$self->data }
 
