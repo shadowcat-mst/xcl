0bd92a843c950038054d9875f6d7a637ee85de62
Matt S Trout <mst@shadowcat.co.uk>
2020-02-08 06:44:34 +0000

    correct result return

--- lib/XCL/V/Var.pm
+++ lib/XCL/V/Var.pm
@@ -6,7 +6,7 @@ use XCL::Class 'XCL::V';
 sub set_data ($self, $new) { $self->data($new); ValF($new) }
 
 sub _invoke ($self, $, $) {
-  ResultF({
+  ResultF(Result {
     val => $self->data,
     set => $self->curry::weak::set_data
   });
