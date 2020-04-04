d5a17b36b5b5443e127ea636d72f8488c14e04b6
Matt S Trout <mst@shadowcat.co.uk>
2020-04-04 00:28:35 +0000

    cleaning ordering of XCL::Class import logic

--- lib/XCL/Class.pm
+++ lib/XCL/Class.pm
@@ -5,26 +5,23 @@ use XCL::Strand::Future;
 use curry;
 use Mojo::Base -strict, -signatures;
 
-sub import ($, $superclass = '-base') {
+sub import ($, $superclass = 'Mojo::Base') {
   unless (caller eq 'XCL::Values') {
     XCL::Values->import::into(1);
   }
-  Safe::Isa->import::into(1);
-  if ($superclass eq '-test') {
+  if ($superclass !~ /^-/) { # normal class
+    Role::Tiny::With->import::into(1);
+    Class::Method::Modifiers->import::into(1);
+  } elsif ($superclass eq '-test') {
     Test2::V0->import::into(1);
     $superclass = '-strict';
-  }
-  if ($superclass eq '-exporter') {
+  } elsif ($superclass eq '-exporter') {
     Exporter->import::into(1, 'import');
     $superclass = '-strict';
   }
+  Safe::Isa->import::into(1);
   Object::Tap->import::into(1);
   Mojo::Base->import::into(1, 'Mojo::Base', $superclass, -signatures);
-  my $is_class = ($superclass eq '-base') || ($superclass !~ /^-/);
-  if ($is_class) {
-    Role::Tiny::With->import::into(1);
-    Class::Method::Modifiers->import::into(1);
-  }
   Future::AsyncAwait->import::into(1, future_class => 'XCL::Strand::Future');
   Syntax::Keyword::Dynamically->import::into(1);
   Syntax::Keyword::Try->import::into(1, qw(try try_value));
