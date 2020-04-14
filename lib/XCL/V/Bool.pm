926eb7c66b5c353805823612af5d3767dea105c3
Matt S Trout <mst@shadowcat.co.uk>
2020-04-14 01:49:51 +0000

    fixups to Bool.not()

--- lib/XCL/V/Bool.pm
+++ lib/XCL/V/Bool.pm
@@ -8,12 +8,13 @@ sub display_data ($self, $) {
   $self->data ? 'true' : 'false'
 }
 
-async sub c_f_not ($self, $lst) {
-  my ($val) = $lst->values;
-  return ValF(Bool(0+!$val->data)) if ref($val) eq '__PACKAGE__';
+async sub c_fx_not ($self, $scope, $lst) {
+  return $_ for not_ok my $vres = await $scope->f_expr($lst);
+  my $val = $vres->val;
+  return Val(Bool(0+!$val->data)) if ref($val) eq '__PACKAGE__';
   my $res = await $val->bool;
   return $res unless $res->is_ok;
-  ValF(Bool(0+!$res->val->data));
+  Val(Bool(0+!$res->val->data));
 }
 
 1;
