213150cc3c5ba12714af1184f07dc7fd4b9c8dae
Matt S Trout <mst@shadowcat.co.uk>
2020-04-24 02:47:16 +0000

    single arg say, add log for stderr

--- lib/xcl/script.pm
+++ lib/xcl/script.pm
@@ -5,7 +5,8 @@ use XCL::Class 'XCL::Inline';
 
 before run => sub ($self) {
   my $scope = $self->scope;
-  $scope->set(say => Val(Native->from_foreign(sub { STDOUT->say(@_) })));
+  $scope->set(say => Val(Native->from_foreign(sub { STDOUT->say($_[0]) })));
+  $scope->set(log => Val(Native->from_foreign(sub { STDERR->say($_[0]) })));
   my $subs = Package::Stash->new($self->package)->get_all_symbols('CODE');
   foreach my $name (grep /^[a-z]/, sort keys %$subs) {
     my $native = Native->from_foreign($subs->{$name});
