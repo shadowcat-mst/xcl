cc8df88357fd799387afec9e8d67f7ba40fba3ff
Matt S Trout <mst@shadowcat.co.uk>
2020-04-24 01:58:16 +0000

    vestigial say until I do better

--- lib/xcl/script.pm
+++ lib/xcl/script.pm
@@ -5,6 +5,7 @@ use XCL::Class 'XCL::Inline';
 
 before run => sub ($self) {
   my $scope = $self->scope;
+  $scope->set(say => Val(Native->from_foreign(sub { STDOUT->say(@_) })));
   my $subs = Package::Stash->new($self->package)->get_all_symbols('CODE');
   foreach my $name (grep /^[a-z]/, sort keys %$subs) {
     my $native = Native->from_foreign($subs->{$name});
